57809d63fe8a9f94c5dc130f5463a8d3cd1049b3
nopgae <123609938+nopgae@users.noreply.github.com>
2024-12-17 10:11:26 -0500

    Update mtcnn.py

--- models/mtcnn.py
+++ models/mtcnn.py
@@ -2,20 +2,11 @@ import torch
 from torch import nn
 import numpy as np
 import os
-
 from .utils.detect_face import detect_face, extract_face
 
-
 class PNet(nn.Module):
-    """MTCNN PNet.
-    
-    Keyword Arguments:
-        pretrained {bool} -- Whether or not to load saved pretrained weights (default: {True})
-    """
-
     def __init__(self, pretrained=True):
         super().__init__()
-
         self.conv1 = nn.Conv2d(3, 10, kernel_size=3)
         self.prelu1 = nn.PReLU(10)
         self.pool1 = nn.MaxPool2d(2, 2, ceil_mode=True)
@@ -26,38 +17,23 @@ class PNet(nn.Module):
         self.conv4_1 = nn.Conv2d(32, 2, kernel_size=1)
         self.softmax4_1 = nn.Softmax(dim=1)
         self.conv4_2 = nn.Conv2d(32, 4, kernel_size=1)
-
-        self.training = False
-
         if pretrained:
             state_dict_path = os.path.join(os.path.dirname(__file__), '../data/pnet.pt')
             state_dict = torch.load(state_dict_path)
             self.load_state_dict(state_dict)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.prelu1(x)
+        x = self.prelu1(self.conv1(x))
         x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.prelu2(x)
-        x = self.conv3(x)
-        x = self.prelu3(x)
-        a = self.conv4_1(x)
-        a = self.softmax4_1(a)
+        x = self.prelu2(self.conv2(x))
+        x = self.prelu3(self.conv3(x))
+        a = self.softmax4_1(self.conv4_1(x))
         b = self.conv4_2(x)
         return b, a
 
-
 class RNet(nn.Module):
-    """MTCNN RNet.
-    
-    Keyword Arguments:
-        pretrained {bool} -- Whether or not to load saved pretrained weights (default: {True})
-    """
-
     def __init__(self, pretrained=True):
         super().__init__()
-
         self.conv1 = nn.Conv2d(3, 28, kernel_size=3)
         self.prelu1 = nn.PReLU(28)
         self.pool1 = nn.MaxPool2d(3, 2, ceil_mode=True)
@@ -71,42 +47,25 @@ class RNet(nn.Module):
         self.dense5_1 = nn.Linear(128, 2)
         self.softmax5_1 = nn.Softmax(dim=1)
         self.dense5_2 = nn.Linear(128, 4)
-
-        self.training = False
-
         if pretrained:
             state_dict_path = os.path.join(os.path.dirname(__file__), '../data/rnet.pt')
             state_dict = torch.load(state_dict_path)
             self.load_state_dict(state_dict)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.prelu1(x)
+        x = self.prelu1(self.conv1(x))
         x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.prelu2(x)
+        x = self.prelu2(self.conv2(x))
         x = self.pool2(x)
-        x = self.conv3(x)
-        x = self.prelu3(x)
-        x = x.permute(0, 3, 2, 1).contiguous()
-        x = self.dense4(x.view(x.shape[0], -1))
-        x = self.prelu4(x)
-        a = self.dense5_1(x)
-        a = self.softmax5_1(a)
+        x = self.prelu3(self.conv3(x))
+        x = self.prelu4(self.dense4(x.view(x.shape[0], -1)))
+        a = self.softmax5_1(self.dense5_1(x))
         b = self.dense5_2(x)
         return b, a
 
-
 class ONet(nn.Module):
-    """MTCNN ONet.
-    
-    Keyword Arguments:
-        pretrained {bool} -- Whether or not to load saved pretrained weights (default: {True})
-    """
-
     def __init__(self, pretrained=True):
         super().__init__()
-
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3)
         self.prelu1 = nn.PReLU(32)
         self.pool1 = nn.MaxPool2d(3, 2, ceil_mode=True)
@@ -124,35 +83,24 @@ class ONet(nn.Module):
         self.softmax6_1 = nn.Softmax(dim=1)
         self.dense6_2 = nn.Linear(256, 4)
         self.dense6_3 = nn.Linear(256, 10)
-
-        self.training = False
-
         if pretrained:
             state_dict_path = os.path.join(os.path.dirname(__file__), '../data/onet.pt')
             state_dict = torch.load(state_dict_path)
             self.load_state_dict(state_dict)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.prelu1(x)
+        x = self.prelu1(self.conv1(x))
         x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.prelu2(x)
+        x = self.prelu2(self.conv2(x))
         x = self.pool2(x)
-        x = self.conv3(x)
-        x = self.prelu3(x)
+        x = self.prelu3(self.conv3(x))
         x = self.pool3(x)
-        x = self.conv4(x)
-        x = self.prelu4(x)
-        x = x.permute(0, 3, 2, 1).contiguous()
-        x = self.dense5(x.view(x.shape[0], -1))
-        x = self.prelu5(x)
-        a = self.dense6_1(x)
-        a = self.softmax6_1(a)
+        x = self.prelu4(self.conv4(x))
+        x = self.prelu5(self.dense5(x.view(x.shape[0], -1)))
+        a = self.softmax6_1(self.dense6_1(x))
         b = self.dense6_2(x)
         c = self.dense6_3(x)
         return b, c, a
-
 
 class MTCNN(nn.Module):
     """MTCNN face detection module.
